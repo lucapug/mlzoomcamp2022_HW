49c54695e608aa49eaaf131fce301d594581e8e3
Luca Pugliese <lucapug@gmail.com>
2022-10-12 11:44:30 +0200

    web service and update predict-test for web

---
+++ predict-test.py
@@ -0,0 +1,20 @@
+import requests
+
+
+url = 'http://localhost:9696/predict_web'
+
+client_id = 'xyz-123'
+client = {"reports": 0, 
+    "share": 0.245, 
+    "expenditure": 3.438, 
+    "owner": "yes"
+}
+
+
+response = requests.post(url, json=client).json()
+print(response)
+
+if response['card_given'] == True:
+    print('Credit card given to %s' % client_id)
+else:
+    print('not emitted card to %s' % client_id)
